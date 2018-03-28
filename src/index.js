05e1a0309aec7b2bcbcb76a9f872be8b6f528f89
abrvsk <abrvsk@gmail.com>
2018-03-28 10:42:52 +0700

    stage 4 linter fix

--- src/index.js
+++ src/index.js
@@ -14,21 +14,21 @@ export const truthCheck = () => {
     }
     return console.log('\nHmm. Very well. Let\'s see if you can pass all the challenges.');
   };
-  const enterState = () => {
-    const first = readlineSync.question('\nAre you ready? ');
-    return first === 'yes' ? firstNum() : result('exitState');
-  };
-  const firstNum = () => {
-    const num1 = readlineSync.question('\nLet\s start then.\nThe first number is 15.\nYour answer is ');
-    return num1 === 'yes' ? secondNum() : result('exitState');
+  const thirdNum = () => {
+    const num3 = readlineSync.question('\nGood. Last number is 1689613.\nYour answer is ');
+    return num3 === 'yes' ? result('succesState') : result('exitState');
   };
   const secondNum = () => {
     const num2 = readlineSync.question('\nOkay. Next number is 22.\nYour answer is ');
     return num2 === 'no' ? thirdNum() : result('exitState');
   };
-  const thirdNum = () => {
-    const num3 = readlineSync.question('\nGood. Last number is 1689613.\nYour answer is ');
-    return num3 === 'yes' ? result('succesState') : result('exitState');
+  const firstNum = () => {
+    const num1 = readlineSync.question('\nLet\'s start then.\nThe first number is 15.\nYour answer is ');
+    return num1 === 'yes' ? secondNum() : result('exitState');
+  };
+  const enterState = () => {
+    const first = readlineSync.question('\nAre you ready? ');
+    return first === 'yes' ? firstNum() : result('exitState');
   };
   enterState();
 };
