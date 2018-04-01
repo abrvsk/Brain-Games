a925bcdc6f2be91d6078784441e43a8b16dd2f31
Andrii Borovskyi <29401099+abrvsk@users.noreply.github.com>
2018-04-01 09:24:20 +0700

    So loops in balance.js

--- src/games/balance.js
+++ src/games/balance.js
@@ -9,19 +9,23 @@ export const balanceTheNumber = (num) => {
     }
     newArray.push(str[acc]);
     return createArray(str, newArray, acc + 1);
-  }
-  let items = createArray(newNum, [], 0);
+  };
+  const items = createArray(newNum, [], 0);
   const last = items.length - 1;
   items.sort();
   const arrayBalancing = (array) => {
     if (Number(array[0]) === (Number(array[last]) - 1) || (array[0] === array[last])) {
       return array;
     }
-    array[0] = Number(array[0]) + 1;
-    array[last] = Number(array[last]) - 1;
+    const a = Number(array[0]) + 1;
+    const b = Number(array[last]) - 1;
+    array.shift();
+    array.unshift(a);
+    array.pop();
+    array.push(b);
     array.sort();
     return arrayBalancing(array);
-  }
+  };
   arrayBalancing(items);
   return items.join('');
 };
