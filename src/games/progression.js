2b3ca01b2ba6407362597f951ae0ca3f2e63bb67
Andrii Borovskyi <29401099+abrvsk@users.noreply.github.com>
2018-04-01 22:19:49 +0700

    Progression game fix: changed getSequence to receive length of requested sequence as an argument

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,21 +1,21 @@
 import { randomNum } from '../brainMath';
 import { gameEngine, pair } from '..';
 
-export const getSequence = () => {
+export const getSequence = (length) => {
   const base = randomNum();
   const diff = randomNum();
   const generateSequence = (newArr, step, acc, prevNum) => {
-    if (acc === 10) {
+    if (acc === 0) {
       return newArr;
     }
     newArr.push(prevNum + step);
-    return generateSequence(newArr, step, acc + 1, prevNum + step);
+    return generateSequence(newArr, step, acc - 1, prevNum + step);
   };
-  return generateSequence([base], diff, 0, base);
+  return generateSequence([base], diff, length, base);
 };
 
 export const progressionGame = () => {
-  const sequence = getSequence();
+  const sequence = getSequence(10);
   const index = Math.floor(randomNum() / 10);
   const correct = sequence[index];
   sequence[index] = '..';
